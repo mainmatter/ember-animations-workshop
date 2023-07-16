861cb259fad35e1ee6dcb559afd8030d3cad50ac
nickschot <nickschot@gmail.com>
2023-07-16 14:12:28 +0200

    Exercise #4 (alternative long solution)

--- app/modifiers/animate-height.js
+++ app/modifiers/animate-height.js
@@ -5,7 +5,26 @@ export default modifier(function animateHeight(
   positional,
   { isOpen = false }
 ) {
-  // short solution
-  let targetHeight = isOpen ? element.scrollHeight : 0;
-  element.style.height = `${targetHeight}px`;
+  // long solution
+
+  // fromHeight is the current height of the element;.
+  let fromHeight = element.clientHeight;
+
+  // toHeight is either 0 (when closing)
+  // or the natural height of the element.
+  let toHeight = 0;
+  if (isOpen) {
+    // Unset the height, so we can get the natural height of the element.
+    element.style.height = 'unset';
+    toHeight = element.clientHeight;
+  }
+
+  // Set the height we want to animate from
+  element.style.height = `${fromHeight}px`;
+
+  // After 1 browser tick, set the target height.
+  // The CSS transition will trigger because of this.
+  setTimeout(() => {
+    element.style.height = `${toHeight}px`;
+  }, 0);
 });
