08ee389cdfeb297f77343e85cf0e6b0c97887063
nickschot <nickschot@gmail.com>
2023-07-16 14:13:34 +0200

    Exercise #5

--- app/components/album-card.js
+++ app/components/album-card.js
@@ -1,8 +1,17 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
+import move from 'ember-animated/motions/move';
+import scale from 'ember-animated/motions/scale';
 
 import './album-card.css';
 
 export default class AlbumCardComponent extends Component {
   @service player;
+
+  *transition({ keptSprites, receivedSprites }) {
+    [...keptSprites, ...receivedSprites].forEach((sprite) => {
+      move(sprite);
+      scale(sprite);
+    });
+  }
 }
